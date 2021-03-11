51eb27b0bd18c6dbc2e6fe16d84eae2622ab4c7a
Zeynep Karakoyun <80275484+zeynepkarakoyun@users.noreply.github.com>
2021-03-11 19:23:19 +0300

    Add files via upload

---
+++ HOMEWORKS/HW2.py
@@ -0,0 +1,14 @@
+CV1 = {"Name" : "Zeynep" , "Surname" : "Karakoyun" , "Age" : "21" , "Degree" : "University" , "English" : "Intermediate"}
+print(CV1)
+
+CV2 = {"Name" : "Gamze" , "Surname" : "Karakoyun" , "Age" : "26" , "Degree" : "Grad" , "English" : "Elemantry"}
+print(CV2)
+
+CV3 = {"Name" : "İsmihan" , "Surname" : "Karakoyun" , "Age" : "28" , "Degree" : "Bachelor" , "English" : "Upper-intermediate" }
+print(CV3)
+
+CV4 = {"Name" : "Murat" , "Surname" : "Demirer" , "Age" : "32" , "Degree" : "Bachelor" , "English" : "Upper-intermediate" }
+print(CV4)
+
+CV5 = {"Name" : "Emre" , "Surname" : "Taşaltın" , "Age" : "22" , "Degree" : "Student" , "English" : "Pre-intermediate" }
+print(CV5)
